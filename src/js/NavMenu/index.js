1f080d6cafef128a634b44784d97c510f4959111
kirillsz <shakhov2013@list.ru>
2024-05-22 19:47:03 +0300

    release: Feature released

---
+++ src/js/NavMenu/index.js
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const [...internalAnchors] =
+    document.getElementsByClassName("anchor-internal");
+  const navMenu = document.querySelector(".qw-nav");
+  const navMenuList = document.querySelector(".qw-nav__list");
+  const navMenuToggleButton = document.querySelector(".qw-nav__toggle");
+
+  function handleMenuState() {}
+  function handleMenuShow() {
+    navMenu.dataset.hidden = "false";
+    navMenuList.classList.remove("hide");
+  }
+  function handleMenuHide() {
+    navMenu.dataset.hidden = "true";
+    navMenuList.classList.add("hide");
+  }
+  function handleMenuToggle() {
+    if (navMenuList.classList.contains("hide")) {
+      handleMenuShow();
+    } else {
+      handleMenuHide();
+    }
+  }
+  internalAnchors.forEach((anchor) =>
+    anchor.addEventListener("click", handleMenuHide)
+  );
+  navMenuToggleButton.addEventListener("click", handleMenuToggle);
+  document
+    .querySelector(".qw-content")
+    .addEventListener("pointerdown", handleMenuHide);
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import "./EntryVideoLoader";
 import "./Loader";
+import "./NavMenu";
 import "./ProductSlider";
 import "./RecomendationSlider";
 import "./3DModel/external";
