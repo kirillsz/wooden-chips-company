076dc47a6e8840539b5afea98fd5eba388cd43a7
kirillsz <shakhov2013@list.ru>
2024-05-27 12:18:01 +0300

    feature: 2x, 3x versions + webp, webm format added

--- src/js/EntryVideoLoader/index.js
+++ src/js/EntryVideoLoader/index.js
@@ -1,14 +1,32 @@
 let tabletBP = 769;
-const videoElement = document.getElementById("entry__video");
+const videoContainer = document.getElementById("entry__video");
 
 const mQuery = window.matchMedia(`(max-width:${tabletBP}px)`);
 
 function handleResize(e) {
   if (!e.matches) {
-    videoElement.src = "videos/company-video.mp4";
+    videoContainer.innerHTML = `<video
+    class="entry-background entry-background--video"
+    autoplay
+    muted
+    loop
+    playsinline
+  >
+  <source src="/videos/company-video.webm" type="video/webm">
+  <source src="/videos/company-video.mp4" type="video/mp4">
+  </video>`;
   }
   if (e.matches) {
-    videoElement.src = "videos/company-video-tablet.mp4";
+    videoContainer.innerHTML = `<video
+    class="entry-background entry-background--video"
+    autoplay
+    muted
+    loop
+    playsinline
+  >
+  <source src="/videos/company-video-tablet.webm" type="video/webm">
+  <source src="/videos/company-video-tablet.mp4" type="video/mp4">
+  </video>`;
   }
 }
 mQuery.addEventListener("change", handleResize);
